e76d5f245e93987cd8d76fac265c15dbda77e98b
swadha94 <103983778+swadha94@users.noreply.github.com>
2023-05-06 16:14:16 +0530

    Update webscraper.py
    Webscraping Covid Website Basics

--- webscraper.py
+++ webscraper.py
@@ -1,30 +1,28 @@
 from flask import Flask , render_template
-
 app = Flask(__name__)
-
 
 from bs4 import BeautifulSoup ## To import Beautiful soup for scraping
 import requests ## To import requests
 
-
 ## Website which is needed to be scraped
-source = requests.get('http://quotes.toscrape.com').text
-soup = BeautifulSoup(source,'lxml')
+url = "https://www.worldometers.info/coronavirus/"
+req = requests.get(url)
+bsObj = BeautifulSoup(req.text, "html.parser")
 
+data=bsObj.find_all('div', class_="maincounter-number")
+totalcases=data[0].text.strip()
+recoveredcases = data[2].text.strip()
 
-quote = soup.find('div', class_='quote')	#finding first quote text
-quotetext = quote.span.text
-print(quotetext)
+totalcases = int(totalcases.replace(',',''))
+recoveredcases = int(recoveredcases.replace(',',''))
 
-author = soup.find('small' , class_= 'author') #finding the first author
-authortext = author.text 
-print(authortext)
+percentagerecovered = recoveredcases*100/totalcases
+percentagerecovered = round(percentagerecovered,2)
 
 @app.route('/')
 
 def home():
-	return render_template('home.html', quotetext = quotetext , authortext = authortext)
-
+	return render_template('home.html',totalcases=totalcases, percentagerecovered=percentagerecovered)
 
 if __name__ == '__main__':
 	app.run(debug=True)
