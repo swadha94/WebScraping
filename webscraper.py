3d940fe10c5e5c295b5db96c08250436f27874e6
swadha94 <103983778+swadha94@users.noreply.github.com>
2023-05-06 15:09:16 +0530

    Update webscraper.py
    This is to use basic web scraping feature on a website to extract author and quotes

--- webscraper.py
+++ webscraper.py
@@ -2,10 +2,28 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
+
+from bs4 import BeautifulSoup ## To import Beautiful soup for scraping
+import requests ## To import requests
+
+
+## Website which is needed to be scraped
+source = requests.get('http://quotes.toscrape.com').text
+soup = BeautifulSoup(source,'lxml')
+
+
+quote = soup.find('div', class_='quote')	#finding first quote text
+quotetext = quote.span.text
+print(quotetext)
+
+author = soup.find('small' , class_= 'author') #finding the first author
+authortext = author.text 
+print(authortext)
+
 @app.route('/')
 
 def home():
-	return render_template('home.html')
+	return render_template('home.html', quotetext = quotetext , authortext = authortext)
 
 
 if __name__ == '__main__':
